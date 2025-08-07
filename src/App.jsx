af69430f912932e6654da90c3cb3dc61918a77cd
Ahmed thabet <ahmedthabet@nsdi.local>
2025-08-07 14:38:58 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import './App.css'
-import MapLayer from './Compontes/Map/MapLayer'
+import React, { useEffect } from 'react';
+import './App.css';
+import MapLayer from './Compontes/Map/MapLayer';
 import Navbar from './Compontes/Map/layout/Navbar';
-
+import { Offline, Online } from 'react-detect-offline';
+import Swal from 'sweetalert2';
 
 function App() {
- 
 
-  return <>
-  <Navbar></Navbar>
-  <MapLayer></MapLayer>
-  </>
+  useEffect(() => {
+    const handleOffline = () => {
+      Swal.fire({
+        title: "No Internet Connection",
+        text: "You are currently offline. Please check your network.",
+        icon: "warning",
+        confirmButtonText: "OK"
+      });
+    };
+
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
+  return (
+    <>
+      <Online>
+        <Navbar />
+        <MapLayer />
+      </Online>
+
+      <Offline>
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+          <h2>You are offline</h2>
+        </div>
+      </Offline>
+    </>
+  );
 }
 
-export default App
+export default App;
